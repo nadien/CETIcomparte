fc5cdae3a56ca4d32ced0c6370106d55584562ca
nadien <sergiomen4@gmail.com>
2015-06-03 00:15:33 -0500

    index.php

--- index.php
+++ index.php
@@ -2,19 +2,19 @@
 	include 'cabecera.php';	
 	include 'includes/config.php';
 	include 'barra_lateral.php';
-	$sql = sprintf("SELECT * FROM articulos ORDER BY id DESC");
-	$res = mysql_query($sql);
-	if (!$res) die('Invalid query: ' . mysql_error());
+	include 'pie.php';
+	
+	$esecuel = mysql_query("SELECT * FROM articulos ORDER BY id DESC");
+	if (!$esecuel) die('Invalid query: ' . mysql_error());
 ?>
 
 	<div id="contenido_izq">
 	
-	<?php while ($post = mysql_fetch_array($res)) { 
+	<?php while ($post = mysql_fetch_array($esecuel)) { 
 		@$id = $post['categoria'];
-		$sql = sprintf("SELECT categoria FROM categorias WHERE id = '$id'");
-		$categoria = mysql_query($sql); 
+		$categoria = mysql_query("SELECT categoria FROM categorias WHERE id = '$id'"); 
 		$categoria = mysql_fetch_array($categoria); ?>
-		<h2><a href="individual.php?id=<?php echo $post['id'] ?>"><?php echo $post['titulo'] ?></a></h2>
+		<h2><a href="individual.php?id=<?php echo $post['id'] ?>"> <?php echo $post['titulo'] ?></a></h2>
 		<p><?php echo $post['contenido'] ?> </p>
 		<p><?php echo $categoria['categoria']; ?></p>
 	<?php } ?>
@@ -23,3 +23,7 @@
 	
 	<?php include 'barra_lateral.php' ?>
 </div>
+
+
+
+
